329034e0224b82976a6b81ac547016893b6c9f9f
Matheus leão <kyracraft0@gmail.com>
2022-04-05 23:51:57 -0300

    faltando apenas o exercicio de número 5

--- Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_7/7.3/sum.js
+++ Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_7/7.3/sum.js
@@ -15,4 +15,34 @@ const myRemove = (arr, item) => {
     return newArr;
 }
 
-module.exports = { sum, myRemove }
+const myFizzBuzz = (num) => {
+    if (typeof num !== 'number') return false;
+    if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+    if (num % 3 === 0) return 'fizz';
+    if (num % 5 === 0) return 'buzz';
+    return num;
+}
+
+const encode = (Codificar) => {
+    for (i = 0; i < Codificar.length; i += 1) {
+        Codificar = Codificar.replace(/e/i, 2);
+        Codificar = Codificar.replace(/a/i, 1);
+        Codificar = Codificar.replace(/i/i, 3);
+        Codificar = Codificar.replace(/o/i, 4);
+        Codificar = Codificar.replace(/u/i, 5);
+    }
+    return Codificar;
+}
+
+const decode = (Decodificar) => {
+    for (i = 0; i < Decodificar.length; i += 1) {
+        Decodificar = Decodificar.replace(/1/i, 'a');
+        Decodificar = Decodificar.replace(/2/i, 'e');
+        Decodificar = Decodificar.replace(/3/i, 'i');
+        Decodificar = Decodificar.replace(/4/i, 'o');
+        Decodificar = Decodificar.replace(/5/i, 'u');
+    }
+    return Decodificar;
+}
+
+module.exports = { sum, myRemove, myFizzBuzz, encode, decode }
--- Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_7/7.3/sum.test.js
+++ Módulo1.Fundamentos.do.desenvolvimento.web/Bloco_7/7.3/sum.test.js
@@ -1,18 +1,71 @@
-const { sum, myRemove } = require('./sum');
+const { sum, myRemove, myFizzBuzz, encode, decode } = require('./sum');
 
-it('A soma de (4, 5) é igual a (9)', () => {
-    expect(sum(4, 5)).toBe(9)
+describe('Testando a função de soma', () => {
+    it('A soma de (4, 5) é igual a (9)', () => {
+        expect(sum(4, 5)).toBe(9)
+    });
+
+    it('A soma de (0, 0) é igual a (0)', () => {
+        expect(sum(0, 0)).toBe(0)
+    });
+
+    it('A soma de (4, String 5) é igual a (Error)', () => {
+        expect(() => sum(4, '5')).toThrowError()
+    });
 });
 
+describe('Testando a função de remover elementos do Array', () => {
+    it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado', () => {
+        expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+    });
 
-it('A soma de (0, 0) é igual a (0)', () => {
-    expect(sum(0, 0)).toBe(0)
+    it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+        expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+    })
+
+    it('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+        expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+    });
 });
 
-it('A soma de (4, String 5) é igual a (Error)', () => {
-    expect(() => sum(4, '5')).toThrowError()
+describe('Testando a função de fizzbuzz para numeros divisivies por 3 ou 5', () => {
+    it('Faça uma chamada com um número divisível por 3 e 5 e verifique se o retorno é o esperado', () => {
+        expect(myFizzBuzz(15)).toBe('fizzbuzz');
+    });
+
+    it('Faça uma chamada com um número divisível por 3 e verifique se o retorno é o esperado', () => {
+        expect(myFizzBuzz(6)).toBe('fizz');
+    });
+
+    it('Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado', () => {
+        expect(myFizzBuzz(10)).toBe('buzz');
+    });
+
+    it('Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado', () => {
+        expect(myFizzBuzz(7)).toBe(7);
+    });
+
+    it('Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado por 3 ou 5 e verifique se o retorno é o esperado', () => {
+        expect(myFizzBuzz('cinco')).toBe(false);
+    });
 });
 
-it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado'), () => {
-    expect(myRemove([1, 2, 3, 4], 3)).toBe([1, 2, 4])
-}
+describe('Testes para as funções de encode e decode', () => {
+    it('Teste se encode e decode são funções', () => {
+        expect(typeof encode).toBe('function')
+        expect(typeof decode).toBe('function')
+    });
+
+    it('Para a função encode teste se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5, respectivamente', () => {
+        expect(encode('a, e, i, o, u')).toEqual('1, 2, 3, 4, 5')
+    });
+
+    it('Para a função decode teste se os números 1, 2, 3, 4 e 5 são convertidos nas vogais a, e, i, o, u , respectivamente', () => {
+        expect(decode('1, 2, 3, 4, 5')).toEqual('a, e, i, o, u')
+    });
+
+    it('Teste se as demais letras/números não são convertidos para cada caso', () => {
+        expect(encode('maria')).toEqual('m1r31')
+        expect(decode('1n1')).toEqual('ana')
+    });
+})
